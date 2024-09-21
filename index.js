6b3c7a280002ed88b0420ddb0064dd9cf4957f12
Victor Souza <victor.souza0350@gmail.com>
2024-09-21 16:19:26 -0300

    feat: added Inquirer, created menu and it's options

---
+++ index.js
@@ -0,0 +1,60 @@
+const { select, number } = require('@inquirer/prompts')
+
+const start = async () => {
+
+    while(true) {
+        const option = await select({
+            message: "Menu >",  /* O select aguarda um obj com exatamente esses props */
+            choices: [
+                {
+                    name: "Adição",
+                    value: "adc"
+                },
+                {
+                    name: "Subtração",
+                    value: "sub"
+                },
+                {
+                    name: "Divisão",
+                    value: "div"
+                },
+                {
+                    name: "Multiplicação",
+                    value: "mult"
+                },
+                {
+                    name: "Porcentagem",
+                    value: "porc"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        }) /* await = aguardar, sempre q usarmos await, temos q ter async no const da função */
+        /* Aqui usamos o await para que o programa espere o usuario escolher uma opção, inves de ficar rodando igual louco */
+
+        switch(option) {
+            case "adc":
+                await adicao()
+                break
+            case "sub":
+                await subtracao()
+                break
+            case "div":
+                await divisao()
+                break
+            case "mult":
+                await multiplicacao()
+                break
+            case "porc":
+                await porcentagem()
+                break
+            case "sair":
+                console.log("Até a proxima!")
+                return
+        }
+    }
+}
+
+start()  
